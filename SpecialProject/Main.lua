c42f0e98b13b3841042c3f27ea80e7740ca51f76
byfronofficial <clarahammersun@gmail.com>
2025-06-12 21:11:40 -0400

    Create Main.lua

---
+++ SpecialProject/Main.lua
@@ -0,0 +1,23 @@
+repeat task.wait() until game:IsLoaded()
+
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+repeat task.wait() until player:FindFirstChild("PlayerGui") and player.PlayerGui:FindFirstChild("MainMenu")
+
+local MainMenu = player.PlayerGui.MainMenu
+repeat task.wait() until MainMenu:FindFirstChild("Main")
+repeat task.wait() until MainMenu.Main:FindFirstChild("Important") and MainMenu.Main:FindFirstChild("Play")
+repeat task.wait() until MainMenu:FindFirstChild("RemoteEvent")
+
+firesignal(MainMenu.Main.Important.OK.MouseButton1Click)
+task.wait(1)
+firesignal(MainMenu.Main.Play.MouseButton1Click)
+task.wait(1)
+
+MainMenu.RemoteEvent:FireServer("LoadChar1")
+repeat task.wait() until player.Character and player.Character:FindFirstChild("HumanoidRootPart")
+
+-- Queue the same script again
+queue_on_teleport("loadstring(game:HttpGet('" .. url .. "', true))()")
+game:GetService("TeleportService"):Teleport((game.PlaceId == 17067673356) and 76053362197989 or 17067673356)
