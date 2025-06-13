2ff0a6099f7c031e07d8bae3e1c890c6394d7c8f
byfronofficial <clarahammersun@gmail.com>
2025-06-12 21:14:15 -0400

    Update Main.lua

--- SpecialProject/Main.lua
+++ SpecialProject/Main.lua
@@ -18,5 +18,6 @@ task.wait(1)
 MainMenu.RemoteEvent:FireServer("LoadChar1")
 repeat task.wait() until player.Character and player.Character:FindFirstChild("HumanoidRootPart")
 
+local url = "https://raw.githubusercontent.com/byfronofficial/Inertia/main/SpecialProject/Main.lua"
 queue_on_teleport("loadstring(game:HttpGet('" .. url .. "', true))()")
 game:GetService("TeleportService"):Teleport((game.PlaceId == 17067673356) and 76053362197989 or 17067673356)
